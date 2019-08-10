efc94ace8637bc2692de7fecdec1cb1a0af92292
winnie <524635843@qq.com>
2019-08-10 17:27:29 +0800

    项目1·

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,56 @@
 <template>
-
-    <div class="app-contain">
-        <mt-header fixed title="dean store"></mt-header>
-
-        <router-view></router-view>
-
-        <nav class="mui-bar mui-bar-tab">
-			<router-link class="mui-tab-item " to="/home">
-				<span class="mui-icon mui-icon-home"></span>
-				<span class="mui-tab-label">首页</span>
-			</router-link>
-			<router-link class="mui-tab-item" to="/member">
-				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">会员</span>
-			</router-link>
-			<router-link class="mui-tab-item" to="/shopcar">
-				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
-                    <span class="mui-badge">0</span>
-                </span>
-				<span class="mui-tab-label">购物车</span>
-			</router-link>
-			<router-link class="mui-tab-item" to="/search">
-				<span class="mui-icon mui-icon-search"></span>
-				<span class="mui-tab-label">搜索</span>
-			</router-link>
-		</nav>
-    </div>
+  <div class="app-contain">
+    <!-- 头部 -->
+    <mt-header fixed title="dean store"></mt-header>
+    <!-- 中部 -->
+    <transition>
+      <router-view></router-view>
+    </transition>
+    <!-- 尾部 -->
+    <nav class="mui-bar mui-bar-tab">
+      <router-link class="mui-tab-item" to="/home">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/member">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/shopcar">
+        <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+          <span class="mui-badge">0</span>
+        </span>
+        <span class="mui-tab-label">购物车</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/search">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </router-link>
+    </nav>
+  </div>
 </template>
 
 
 
 <script>
-
 </script>
 <style lang='scss' scoped>
-.app-contain{
-    padding-top: 40px;
+.app-contain {
+  padding-top: 40px;
+  overflow-x: hidden;
 }
-
+.v-enter{
+	opacity: 0;
+	transform: translateX(100%);
+}
+.v-leave-to{
+	opacity: 0;
+	transform: translateX(-100%);
+	position: absolute;
+}
+.v-enter-active,
+.v-leave-active{
+	transition: all 0.5s ease;
+}
 </style>
 
--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 
 var router=new VueRouter({
     routes:[
+         {path: '/',redirect:'/home'},
         { path: '/home',component:HomeContainer},
         { path: '/member',component:MemberContainer},
         { path: '/search',component:SearchContainer},
